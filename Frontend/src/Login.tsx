8e2cccfff56aad55c071ab9bb1049f9c961d22e8
takacsvilmos <vilmos.takacs@gmail.com>
2024-10-14 14:10:47 +0200

    login screen inputs added

--- Frontend/src/Login.tsx
+++ Frontend/src/Login.tsx
@@ -10,6 +10,13 @@ const Login = () => {
     return (
         <div>
             <p>Login page</p>
+            <form action="">
+                <label htmlFor="username">username:</label>
+                <input name="username" type="text" />
+                <label htmlFor="password">password:</label>
+                <input name="password" type="text" />
+                <button>Login</button>
+            </form>
             <button onClick={handleClick}>Back to home</button>
         </div>
     );
