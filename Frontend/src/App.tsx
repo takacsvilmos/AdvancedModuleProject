345ff9898b09ed68625e2af6a7d78dfa1d38680d
DanielOzmin <benedek.deniel@gmail.com>
2024-10-17 11:13:00 +0200

    SignUp basics

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Home from './Home'
 import Login from './Login'
+import SignUp from './SignUp'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element ={<Home/>}/>
           <Route path='/login' element ={<Login/>}/>
+          <Route path='/signup' element ={<SignUp/>}/>
         </Routes>
       </div>
     </>
