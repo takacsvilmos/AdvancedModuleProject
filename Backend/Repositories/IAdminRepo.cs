2c5394da8d8eb65dec38658d27445f082d6a947f
takacsvilmos <vilmos.takacs@gmail.com>
2024-10-17 15:17:02 +0200

    Extension on adminRepo

--- Backend/Repositories/IAdminRepo.cs
+++ Backend/Repositories/IAdminRepo.cs
@@ -1,6 +1,8 @@
-namespace Backend.Repositories;
-
 public interface IAdminRepo
 {
     List<string> GetAllUsers();
+    string GetUserById();
+    string UpdateUserById();
+    string CreateUserByAdmin();
+    string DeleteUserById();
 }
