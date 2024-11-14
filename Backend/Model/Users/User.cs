c4dfe8924d0aef8b88a3e29ac0b09c5ef303706a
DanielOzmin <benedek.deniel@gmail.com>
2024-11-14 14:41:58 +0100

    features/login, signup to db & frontend

--- Backend/Model/Users/User.cs
+++ Backend/Model/Users/User.cs
@@ -6,7 +6,7 @@ public class User
 {
     [Key]
     public Guid _id { get; set; }
-    public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
+    public string Role { get; set; }
 }
