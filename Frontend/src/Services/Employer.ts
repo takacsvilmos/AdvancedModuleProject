a8c44dfb15ee7ea17848d7e588b111503f8d44db
JonBorka <jon.buzasi@gmail.com>
2024-12-11 16:37:04 +0100

    "merging to own branch"

---
+++ Frontend/src/Services/Employer.ts
@@ -0,0 +1,36 @@
+import { createContext } from "react";
+import JobOffer from "../Components/JobOffer/JobOffer";
+
+export type Listing = {
+    id: number;
+    company_id: number;
+    company_name: string;
+    field: string[];
+    location: string;
+    req_work_exp: number;
+    salary: number;
+    working_hours: number;
+    phone: number;
+    email: string;
+    description: string;
+    image: string | null;
+}
+
+export type Employer = {
+  id: number;
+  company_name: string;
+  field: string[];
+  address: string | null;
+  phone: number;
+  email: string;
+  description: string | null;
+  job_offers: Listing[];
+  image: string | null; 
+};
+
+export type EmployerContextType = {
+  employer: Employer;
+  setEmployer: React.Dispatch<React.SetStateAction<Employer>>;
+};
+
+export const UserContext = createContext<EmployerContextType | null>(null);
